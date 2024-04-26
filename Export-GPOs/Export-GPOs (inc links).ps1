bf9995ec4735ca7f6b052c1ac30d194216239f34
Richard Easton <richeaston@users.noreply.github.com>
2024-04-26 09:32:20 +0100

    Update Export-GPOs (inc links).ps1
    Added filter to only show LinkEnabled = False, and TargetOU = ""
    
    Also renamed some of the properties in the PSCustomObject

--- Export-GPOs/Export-GPOs (inc links).ps1
+++ Export-GPOs/Export-GPOs (inc links).ps1
@@ -12,20 +12,28 @@ $gpos = $OUs | ForEach-Object { (Get-GPInheritance -Target $_ -ErrorAction Silen
 
 foreach ($gpo in $gpos) {
     try {
+    Write-host "Processing $($GPO.DisplayName)"
     $Comment = Get-GPO -Name $gpo.displayname -ErrorAction SilentlyContinue | Select-Object  Description, gpostatus, owner
     $OUTGPO = [PSCustomObject] @{
-        'Name' = $gpo.DisplayName
-        'Enabled' = $GPO.Enabled
-        'Description' = $comment.description
-        'Target' = $gpo.Target
-        'status' = $comment.gpostatus
-        'owner' = $comment.owner
-        'domain' = $domain
+        Name = $gpo.DisplayName
+        LinkEnabled = $GPO.Enabled
+        #Description = $comment.description
+        TargetOU = $gpo.Target
+        GPOStatus = $comment.gpostatus
+        Owner = $comment.owner
+        Domain = $domain
     }
-    $array += $OUTGPO
+        #use this script block to only show disabled or unlinked gpo's
+        if ($($outgpo.TargetOU) -eq "" -or $($outgpo.LinkEnabled) -eq $false) {
+            $array += $OUTGPO
+        }
+        
+        #use this scriptblock to show all gpo's
+        #$array += $OUTGPO
+        
     }
     catch {
-        Write-Warning "An error occurred getting GPO data!"
+        Write-Warning "An error occurred getting $($GPO.DisplayName) data!"
     }
     
 }
