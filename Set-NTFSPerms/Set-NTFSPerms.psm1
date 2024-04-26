45a457486ff51aaa50bdf28f0e8da626ad26524a
Richard Easton <richeaston@users.noreply.github.com>
2024-04-26 12:48:26 +0100

    Update Set-NTFSPerms.psm1
    update syntax

--- Set-NTFSPerms/Set-NTFSPerms.psm1
+++ Set-NTFSPerms/Set-NTFSPerms.psm1
@@ -1,9 +1,13 @@
-Function SetNTFS-Perm($parent, $foldername, $groupname) {
-    New-item -Path $parent -Name $foldername -ItemType Directory -force -Verbose
-    $path = "$parent\$foldername"
+Function Set-NTFSPerm($parent, $foldername, $groupname) {
+    #can be used to create the folder if it doesn't exist
+    #New-item -Path $parent -Name $foldername -ItemType Directory -force -Verbose
+    $path = join-path -path $parent -childpath $foldername
     $Acl = Get-Acl $path
     $permission = $groupname, 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow'
     $AR = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permission
     $Acl.SetAccessRule($Ar)
     Set-Acl $path $Acl
 }
+
+#usage
+#Set-ntfsperms -parent "c:\temp" -folder "myfolder" -groupname "domain\groupname"
