4dab04434819c8c768bcaa73ae476b9ed2013b80
Richard Easton <richeaston@users.noreply.github.com>
2024-05-16 14:03:46 +0100

    Winget Backup and Restore Local Apps

---
+++ Winget/Backup-WingetApp.ps1
@@ -0,0 +1,28 @@
+﻿Clear-host
+$results = @()
+$wingetOutput = (winget list) -split '[\r\n]'
+$wingetOutput | ForEach-Object {
+    if (!($_ -like '*  - \ |  *' -or $_ -like '   - \ *' -or $_ -like '   - *' -or $_ -like '*upgrades available' -OR $_ -like '*-*' -or $_ -eq $null -or $_ -eq '' )) {
+        if ($_.Startswith("Name")) {
+            $id = $_.indexof('Id')
+            $Ver = $_.indexof('Version')
+            $source = $_.indexof('Source')
+        }
+        else {
+            if ($_ -ne $null -and $_ -ne " ") {
+                $item = [PSCustomObject]@{
+                    Name    = $_.Substring(0, $id).TrimEnd()
+                    ID      = $_.Substring($id, ($ver - $id)).TrimEnd()
+                    Version = $_.Substring($ver, ($source - $ver)).TrimEnd()
+                    Source  = $_.Substring($source, ($_.length - $source)).TrimEnd()
+                }
+                if ($item.source -eq 'winget' -or $item.source -eq 'msstore') {
+                    $results += $Item
+                }
+            }    
+        }
+    }
+
+}
+if (test-path .\My-Installed-apps.csv) {remove-item -Path .\My-Installed-apps.csv -force}
+$results | export-csv ".\My-Installed-apps.csv" -NoClobber -NoTypeInformation -Force
---
+++ Winget/Install apps.ps1
@@ -0,0 +1,18 @@
+﻿Clear-Host
+$excluded = @('onedrive', 'edge', 'ui.xaml')
+
+foreach ($app in (Import-Csv -Path .\My-Installed-apps.csv)) {
+    if ($app.Name -notmatch ($excluded -join '|')) {
+        Write-Host "Searching for " -NoNewline -ForegroundColor Yellow
+        Write-Host $app.Name -ForegroundColor Cyan -NoNewline
+        Write-Host ", Please Wait..." -ForegroundColor Yellow
+        $appsearch = winget search --id $app.ID --exact
+        if ($appsearch) {
+            Write-Output $appsearch
+            Winget install --id $app.ID --exact --silent --accept-package-agreements --accept-source-agreements
+            Write-Host ""
+        }
+    } else {
+        Write-Host "$($app.Name) Excluded.!" -ForegroundColor Magenta
+    }
+}
